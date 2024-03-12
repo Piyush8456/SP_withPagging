c50e86714d22b07ca8c38ffbbb35ff92622444ca
Piyush8456 <P8use9@gmail.com>
2024-03-12 19:15:22 +0530

    first-commit

---
+++ gettingData/Controllers/EmployeesController.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using gettingData.Services;
+using gettingData.Models;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+
+
+namespace gettingData.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        private readonly _empServices _db;
+
+        public EmployeesController()
+        {
+            _db = new _empServices(); 
+        }
+ 
+        public ActionResult Index()
+        {
+            var employees = _db.Database.SqlQuery<employeeEntity>("EXEC getData_SP").ToList();
+
+           
+
+            return View(employees);
+        }
+
+        [HttpPost]
+        public JsonResult AjaxMethod(int pageIndex, string searchTerm)
+        {
+            employeeEntityModel model = new employeeEntityModel();
+            model.PageIndex = pageIndex;
+            model.PageSize = 10;
+
+            List<employeeEntity> employees = new List<employeeEntity>();
+            string constring = ConfigurationManager.ConnectionStrings["constr"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(constring))
+            {
+                using (SqlCommand cmd = new SqlCommand("GetCustomersPageWise", con))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@PageIndex", model.PageIndex);
+                    cmd.Parameters.AddWithValue("@PageSize", model.PageSize);
+                    con.Open();
+                    SqlDataReader sdr = cmd.ExecuteReader();
+                    while (sdr.Read())
+                    {
+                        employees.Add(new employeeEntity
+                        {
+                            employeeId = (int)sdr["employeeId"],
+                            firstName = sdr["firstName"].ToString(),
+                            lastName = sdr["lastName"].ToString(),
+                            mobileNumber = sdr["mobileNumber"].ToString()
+                        });
+                    }
+                    con.Close();
+
+                    model.employees  = employees;
+                }
+            }
+
+            return Json(model);
+        }
+    }
+
+}
---
+++ gettingData/Models/employeeEntity.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace gettingData.Models
+{
+
+    public class employeeEntityModel
+    {
+        public List<employeeEntity> employees { get; set; }
+        public int PageIndex { get; set; }
+        public int PageSize { get; set; }
+    }
+
+    public class employeeEntity
+    {
+        [Key]
+        [DisplayName("employeeId")]
+        public int employeeId { get; set; }
+
+        [Required]
+        [DisplayName("firstName")]
+        public string firstName { get; set; }
+
+        [Required]
+        [DisplayName("lastName")]
+        public string lastName { get; set; }
+
+        [Required]
+        [DisplayName("mobileNumber")]
+        public string mobileNumber { get; set; }
+    }
+}
